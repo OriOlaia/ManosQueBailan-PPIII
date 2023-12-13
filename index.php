5674da1cb450329a34a77de14fe78642bb64c477
Ori Olaia <oriana.olaia17@gmail.com>
2023-12-13 06:22:00 -0300

    Renovando todo de 0

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manos que Bailan</title>
+</head>
+<body>
+    <header>
+
+    </header>
+    <section>
+        <h2>Bienvenidos a este curso</h2>
+        <a href="login.php">Empezar</a>
+    </section>
+    <footer>
+
+    </footer>
+</body>
+</html>
